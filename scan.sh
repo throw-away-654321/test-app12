0c647c9e4411fb67181a42bc20eadf66ad6e33f7
bigspotteddog <bink.lynch@gmail.com>
2019-07-08 12:18:26 -0700

    Increment commit hash.

--- scan.sh
+++ scan.sh
@@ -1,2 +1,2 @@
 export COMMIT=$(git rev-parse HEAD); echo "{\"commitHash\": \"${COMMIT}\"}" > metadata.json
-java -jar /Users/jcava/Documents/workspace/insight/insight-brain/nexus-iq-cli/target/nexus-iq-cli-1.69.0-SNAPSHOT.jar -i test-app -s http://localhost:8070 -a admin:admin123 -t release -m metadata.json ./target/lib
+java -jar /Users/jcava/Downloads/nexus-iq-server-1.68.0-01-bundle/nexus-iq-cli-1.68.0-01.jar -i test-app -s http://localhost:8070 -a admin:admin123 -t release -m metadata.json ./target/lib
