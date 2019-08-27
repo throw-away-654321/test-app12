eee6f272b1cd12f734bedd0caff3ddebc520e75a
bigspotteddog <bink.lynch@gmail.com>
2019-08-27 15:00:38 -0700

    Bump.

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 # test-app
 
-## a change
-
-### version bump
+This is a test
--- scan.sh
+++ scan.sh
@@ -1,2 +1,2 @@
 export COMMIT=$(git rev-parse HEAD); echo "{\"commitHash\": \"${COMMIT}\"}" > metadata.json
-java -jar /Users/jcava/Documents/workspace/insight/insight-brain/nexus-iq-cli/target/nexus-iq-cli-1.69.0-SNAPSHOT.jar -i test-app -s http://localhost:8070 -a admin:admin123 -t release -m metadata.json ./target/lib
+java -jar /Users/jcava/Documents/workspace/insight/insight-brain/nexus-iq-cli/target/nexus-iq-cli-1.67.0-SNAPSHOT.jar -i test-app -s http://localhost:8070 -a admin:admin123 -t release -m metadata.json ./target/lib
